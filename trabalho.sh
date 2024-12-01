e42e3ac9f119e857062531875796afeb419a0712
Eduardo Medel <127570392+medelzin@users.noreply.github.com>
2024-12-01 18:23:30 -0300

    trabalho feito

--- trabalho.sh
+++ trabalho.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+#1
+sudo apt install -y fortune
+
+#2
+fortune > mensagem1.txt
+fortune > mensagem2.txt
+fortune > mensagem3.txt
+
+#3
+cat mensagem1.txt mensagem2.txt mensagem3.txt > mensagens.txt
+
+#4
+cp mensagens.txt mensagens_antiga.txt
+fortune >> mensagens.txt
+
+#5
+wc -l mensagens.txt | awk '{print $1}' > linhas.txt
+
+#6
+egrep -o 'a' mensagens.txt | wc -l > letras.txt
+
+#7
+ls -R /etc 1> saida.txt 2> erros.txt
+
+#8
+ls -R /etc > saida_com_erros.txt
+
+#9
+cat /etc/passwd | cut -d ":" -f 1 > usuarios.txt
+
+#10
+cat /etc/passwd | cut -d ":" -f 1 | sort > usuarios_ordenados.txt
